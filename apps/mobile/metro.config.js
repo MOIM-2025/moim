3a24d92420bf2eb4877670aaea8b10bb7c453a87
DongGeunYoon <ehdrmsdl9999@naver.com>
2025-10-30 21:53:48 +0900

    [config] Metro 패키지 추가 및 설정 - Metro 패키지 추가 및 설정

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,11 +1,25 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
+const monorepoRoot = path.resolve(__dirname, '../..');
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  watchFolders: [
+    monorepoRoot,
+    `${monorepoRoot}/packages/ui`,
+    `${monorepoRoot}/packages/ts-config`,
+  ],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(monorepoRoot, 'node_modules'),
+    ],
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
